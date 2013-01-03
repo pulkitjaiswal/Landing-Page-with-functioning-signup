ff6af547310f12ae0ca4286a5b482205188f3563
Bui Thanh Hai <hai.bth@gmail.com>
2013-01-03 07:18:01 +0700

    Fix assets loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,9 +13,10 @@ Signup::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.debug = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
